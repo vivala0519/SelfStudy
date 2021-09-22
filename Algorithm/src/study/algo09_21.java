fd6207377995ad9ad4728850a55c2d9fdef8a1ad
Dzeko <vivala0519@naver.com>
2021-09-22 23:20:50 +0900

    70th Algo

---
+++ Algorithm/src/study/algo09_21.java
@@ -0,0 +1,13 @@
+package study;
+
+import java.util.Arrays;
+
+public class algo09_21 {
+
+	public static void main(String[] args) {
+		String str = "Pig latin is cool";
+		String[] arr = str.split(" ");
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
---
+++ Algorithm/src/study/algo09_22.java
@@ -0,0 +1,10 @@
+package study;
+
+public class algo09_22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
