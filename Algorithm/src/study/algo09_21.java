247185f1ceeed925bcaea934f3c9799bf2d2072e
Dzeko <vivala0519@naver.com>
2021-09-23 00:38:10 +0900

    71th Algo

--- Algorithm/src/study/algo09_21.java
+++ Algorithm/src/study/algo09_21.java
@@ -5,9 +5,22 @@ import java.util.Arrays;
 public class algo09_21 {
 
 	public static void main(String[] args) {
-		String str = "Pig latin is cool";
+		String str = "Hello world !";
 		String[] arr = str.split(" ");
+		String result = "";
+		System.out.println(arr[0].substring(0, 1));
+		System.out.println(arr[0].substring(1));
+		for(int i = 0; i < arr.length; i++) {
+			if(!arr[i].matches("[^a-zA-Z0-9\\\\s]")) {
+				arr[i] = arr[i].substring(1) + arr[i].substring(0, 1) + "ay";
+			}
+			result += arr[i];
+			if(i < arr.length - 1) {
+				result += " ";
+			}
+		}
 		System.out.println(Arrays.toString(arr));
+		System.out.println(result);
 	}
 
 }
--- Algorithm/src/study/algo09_22.java
+++ Algorithm/src/study/algo09_22.java
@@ -3,8 +3,15 @@ package study;
 public class algo09_22 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str = "samurai";
+		String ending = "ra";
+		System.out.println(str.substring(str.length()-ending.length(), str.length()));
+		if(str.substring(str.length()-ending.length(), str.length()).contains(ending)) {
+			System.out.println("포함");
+		}
+		else {
+			System.out.println("미포함");
+		}
 	}
 
 }
