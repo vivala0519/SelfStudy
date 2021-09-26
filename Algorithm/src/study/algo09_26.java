b8f7c52f70bec2341ab6610577a15eb06e395ed0
Dzeko <vivala0519@naver.com>
2021-09-27 00:00:57 +0900

    75th Algo

--- Algorithm/src/study/algo09_26.java
+++ Algorithm/src/study/algo09_26.java
@@ -4,18 +4,24 @@ public class algo09_26 {
 
 	public static void main(String[] args) {
 		double principal = 1000;
-		double interst = 0.05;
+		double interst = 0.01625;
 		double tax = 0.18;
-		double desired = 1100;
-		int i = 1;
+		double desired = 1000;
+		int i = 0;
 		while(principal < desired) {
-			System.out.println(i);
-			double cal = principal * (1 + interst) - (principal * (1 + interst) - principal) * tax;
-			System.out.println(cal);
+			double cal = principal * (1 + interst) - (principal * interst) * tax;
 			principal = cal;
 			i ++;
 			System.out.println(cal);
 		}
+		System.out.println(i);
+//		principal = 1041.00;
+//		System.out.println(principal * (1 + interst));
+//		System.out.println(principal * (1 + interst) - principal);
+//		System.out.println(principal * (1 + interst) - (principal * interst) * tax);
+		
+		
+		
 //		for(int i = 1; principal > desired; i++) {
 //			double cal = principal * (1 + interst) - (principal * (1 + interst) - principal) * tax;
 //			System.out.println(cal);
