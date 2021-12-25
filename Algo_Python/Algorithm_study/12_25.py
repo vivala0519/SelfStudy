0f540482be8deb2e2444783d69d1a548efabf80d
vivala0519 <42686784+vivala0519@users.noreply.github.com>
2021-12-25 20:57:08 +0900

    156th Algo

---
+++ Algo_Python/Algorithm_study/12_25.py
@@ -0,0 +1,9 @@
+dad_years_old = 29
+son_years_old = 0
+
+if son_years_old * 2 == dad_years_old:
+    print(0)
+else:
+    dad_years_old = dad_years_old - son_years_old
+    result = dad_years_old - son_years_old
+    print(abs(result))
