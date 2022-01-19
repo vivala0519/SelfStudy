79e834a776a5d73c9da77e12a5855e0455510808
Dzeko <vivala0519@naver.com>
2022-01-19 19:13:32 +0900

    hash_map_structures

---
+++ Algo_Python/Algorithm_study/hash_map/structures.py
@@ -0,0 +1,59 @@
+import collections
+
+
+class MyHashMap:
+    def __init__(self):
+        self.size = 1000
+        self.table = collections.defaultdict(ListNode)
+
+    def put(self, key: int, value: int) -> None:
+        index = key % self.size
+        # 인덱스에 노드가 없다면 삽입 후 종료
+        if self.table[index].value is None:
+            self.table[index] = ListNode(key, value)
+            return
+        # 인덱스에 노드가 존재하는 경우 연결 리스트 처리
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                p.value = value
+                return
+            if p.next is None:
+                break
+            p = p.next
+        p.next = ListNode(key, value)
+
+    def get(self, key: int) -> int:
+        index = key % self.size
+        if self.table[index].value is None:
+            return -1
+
+        # 노드가 존재할 때 일치하는 키 탐색
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                return p.value
+            p = p.next
+        return -1
+
+    def remove(self, key: int) -> None:
+        index = key % self.size
+        if self.table[index].value is None:
+            return
+
+        p = self.table[index]
+        if p.key == key:
+            self.table[index] = ListNode() if p.next is None else p.next
+            return
+
+        prev = p
+        while p:
+            if p.key == key:
+                prev.next = p.next
+                return
+            prev, p = p, p.next
+class ListNode:
+    def __init__(self, key=None, value=None):
+        self.key = key
+        self.value = value
+        self.next = None
