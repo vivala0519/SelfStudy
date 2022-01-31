47d873a878d14bb35d15e70798ead76f259c8acf
Dzeko <vivala0519@naver.com>
2022-01-31 14:46:42 +0900

    quick sort

--- Algo_Python/Algorithm_study/sort/sorts.py
+++ Algo_Python/Algorithm_study/sort/sorts.py
@@ -46,3 +46,22 @@ def insertionsort_2(lst):
         lst[cmp + 1] = val
     
     return lst
+
+def quicksort(lst, start, end):
+    def partition(part, ps, pe):
+        pivot = part[pe]
+        i = ps - 1
+        for j in range(ps, pe):
+            if part[j] <= pivot:
+                i += 1
+                part[i], part[j] = part[j], part[i]
+        part[i + 1], part[pe] = part[pe], part[i + 1]
+        return i + 1
+    
+    if start >= end:
+        return None
+    
+    p = partition(lst, start, end)
+    quicksort(lst, start, p - 1)
+    quicksort(lst, p + 1, end)
+    return lst
