8146a4f28c472c24d363a81a684bad3a8ba3c0e9
Dzeko <vivala0519@naver.com>
2022-02-01 22:57:05 +0900

    merge sort

--- Algo_Python/Algorithm_study/sort/sorts.py
+++ Algo_Python/Algorithm_study/sort/sorts.py
@@ -65,3 +65,33 @@ def quicksort(lst, start, end):
     quicksort(lst, start, p - 1)
     quicksort(lst, p + 1, end)
     return lst
+
+def merge(arr1, arr2):
+    result = []
+    i = j = 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            result.append(arr1[i])
+            i += 1
+        else:
+            result.append(arr2[j])
+            j += 1
+    
+    while i < len(arr1):
+        result.append(arr1[i])
+        i += 1
+
+    while j < len(arr2):
+        result.append(arr2[j])
+        j += 1
+
+    return result
+
+def merge_sort(lst):
+    if len(lst) <= 1:
+        return lst
+
+    mid = len(lst) // 2
+    L = lst[:mid]
+    R = lst[mid:]
+    return merge(merge_sort(L), merge_sort(R))
--- Algo_Python/Algorithm_study/sort/test_sort.py
+++ Algo_Python/Algorithm_study/sort/test_sort.py
@@ -1,4 +1,4 @@
-from Algorithm_study.sort.sorts import bubblesort, selectionsort, insertionsort, insertionsort_2, quicksort
+from Algorithm_study.sort.sorts import bubblesort, selectionsort, insertionsort, insertionsort_2, quicksort, merge_sort
 
 assert bubblesort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
 assert bubblesort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
@@ -22,3 +22,6 @@ assert quicksort([3, 2, 1, 5, 3, 2, 3], 0, 3) == [1, 2, 3, 5, 3, 2, 3]
 assert quicksort([3, 2, 1, 5, 3, 2, 3], 0, 4) == [1, 2, 3, 3, 5, 2, 3]
 assert quicksort([3, 2, 1, 5, 3, 2, 3], 0, 5) == [1, 2, 2, 3, 3, 5, 3]
 assert quicksort([3, 2, 1, 5, 3, 2, 3], 0, 6) == [1, 2, 2, 3, 3, 3, 5]
+
+assert merge_sort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
+assert merge_sort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
