5b8028866e9e7bed9aeff22d610aa648b94195c0
Dzeko <42686784+vivala0519@users.noreply.github.com>
2022-03-11 02:38:58 +0900

    232nd Algo

---
+++ Algo_Python/Algorithm_study/220311_boj_1929.py
@@ -0,0 +1,18 @@
+import math, sys
+
+
+def isPrime(num):
+    if num == 1:
+        return False
+
+    for i in range(2, int(math.sqrt(num))+1):
+        if num % i == 0:
+            return False
+
+    return True
+
+M, N = map(int, sys.stdin.readline().rstrip().split())
+
+for i in range(M, N + 1):
+    if isPrime(i):
+        print(i)
