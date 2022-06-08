fa6a3e6f6ce0e56913b03093d9eec28e9b77f493
Dzeko <42686784+vivala0519@users.noreply.github.com>
2022-06-09 00:42:59 +0900

    322th Algo

---
+++ Algo_Python/Algorithm_study/220609_boj_2178.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+
+    while q:
+        x, y = q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or ny < 0 or nx >= N or ny >= M:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                q.append((nx, ny))
+    return graph[N - 1][M - 1]
+
+
+N, M = map(int, sys.stdin.readline().split())
+
+graph = []
+for i in range(N):
+    graph.append(list(map(int, sys.stdin.readline().rstrip())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+
+print(bfs(0,0))
