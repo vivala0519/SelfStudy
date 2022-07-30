0a567890daf0f818f8f8b671c90b7271ac53efc2
Dzeko <42686784+vivala0519@users.noreply.github.com>
2022-07-30 23:38:09 +0900

    372nd Algo

---
+++ Algo_Python/Algorithm_study/220730_boj_9663.py
@@ -0,0 +1,26 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+result = 0
+row = [0] * n
+
+def process(x):
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x] - row[i]) == abs(x - i):
+            return False
+    return True
+
+def n_queens(x):
+    global result
+
+    if x == n:
+        result += 1
+    else:
+        for i in range(n):
+            row[x] = i
+            if process(x):
+                n_queens(x + 1)
+
+n_queens(0)
+print(result)
